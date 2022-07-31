77fa081dd012744e03a838f26d2cad1a82b41ae9
danielmpedrozo <108821459+danielmpedrozo@users.noreply.github.com>
2022-07-31 19:58:13 -0300

    ok

--- main.py
+++ main.py
@@ -12,13 +12,12 @@ mp_pose2 = mp.solutions.pose
 cap1 = cv2.VideoCapture(0)
 cap2 = cv2.VideoCapture(1)
 
-
-
 # Setup mediapipe instance
-results = 0
-keypoints = []
+keypoints1 = []
+keypoints2 = []
 
-with mp_pose1.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.2) as pose1, mp_pose2.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.2) as pose2:
+with mp_pose1.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.2) as pose1, \
+        mp_pose2.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.2) as pose2:
     while cap1.isOpened() and cap2.isOpened():
         ret1, frame1 = cap1.read()
         ret2, frame2 = cap2.read()
@@ -30,14 +29,6 @@ with mp_pose1.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.2) as
         image2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2RGB)
         image2.flags.writeable = False
 
-        # Make detection
-
-        # if results == 0:
-        #     results = pose.process(image)
-        # elif results != pose.process(image):
-        #     break
-        # else:
-
         results1 = pose1.process(image1)
         results2 = pose2.process(image2)
 
@@ -48,106 +39,54 @@ with mp_pose1.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.2) as
         image2 = cv2.cvtColor(image2, cv2.COLOR_RGB2BGR)
 
         # Extract landmarks
-        try:
-            landmarks = results2.pose_landmarks.landmark
-            #print(landmarks[0])
-        except:
-            pass
-
-        # for lndnmrk in mp_pose1.PoseLandmark:
-        #     print(lndnmrk)
-
-        pos_nariz = landmarks[mp_pose1.PoseLandmark.NOSE]
-
-        # if cont == 0:
-        #     x_nariz = pos_nariz.x
-        # else:
-        #     if float(x_nariz) - 0.2 > float(pos_nariz.x) or float(pos_nariz) > float(x_nariz + 0.2):
-        #         break
-
-        # print('---------')
-        # print(pos_nariz.x)
-        # print(x_nariz)
-        # print('---------')
-        #
-
+        landmarks2 = results2.pose_landmarks.landmark
 
         # Render detections
         mp_drawing1.draw_landmarks(image1, results1.pose_landmarks, mp_pose1.POSE_CONNECTIONS,
-                                  mp_drawing1.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
-                                  mp_drawing1.DrawingSpec(color=(66, 245, 66), thickness=2, circle_radius=2))
+                                   mp_drawing1.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                                   mp_drawing1.DrawingSpec(color=(66, 245, 66), thickness=2, circle_radius=2))
 
         mp_drawing2.draw_landmarks(image2, results2.pose_landmarks, mp_pose2.POSE_CONNECTIONS,
-                                  mp_drawing2.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
-                                  mp_drawing2.DrawingSpec(color=(66, 245, 66), thickness=2, circle_radius=2))
+                                   mp_drawing2.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                                   mp_drawing2.DrawingSpec(color=(66, 245, 66), thickness=2, circle_radius=2))
 
-        cont = 0
-        keypoints_movel = []
-        sair = False
-        for data_point in landmarks:
-            keypoints_movel.append({
+        # Motion detection
+        keypoints_movel2 = []
+        sair2 = False
+        for data_point in landmarks2:
+            keypoints_movel2.append({
                 'X': data_point.x,
                 'Y': data_point.y,
                 'Z': data_point.z,
             })
 
-        if len(keypoints) == 0:
-            for data_point in landmarks:
-                    keypoints.append({
+        if len(keypoints2) == 0:
+            for data_point in landmarks2:
+                keypoints2.append({
                         'X': data_point.x,
                         'Y': data_point.y,
                         'Z': data_point.z,
                         })
         else:
-            for c in range(0, len(keypoints)):
-                if keypoints[c]['X'] - 0.08 < keypoints_movel[c]['X'] < keypoints[c]['X'] + 0.08 and keypoints[c]['Y'] - 0.1 < keypoints_movel[c]['Y'] < keypoints[c]['Y'] + 0.1 and keypoints[c]['Z'] - 0.4 < keypoints_movel[c]['Z'] < keypoints[c]['Z'] + 0.4:
-                    keypoints[c]['X'] = keypoints_movel[c]['X']
-                    keypoints[c]['Y'] = keypoints_movel[c]['Y']
-                    keypoints[c]['Z'] = keypoints_movel[c]['Z']
+            for c in range(0, len(keypoints2)):
+                if keypoints2[c]['X'] - 0.08 < keypoints_movel2[c]['X'] < keypoints2[c]['X'] + 0.08 and \
+                        keypoints2[c]['Y'] - 0.1 < keypoints_movel2[c]['Y'] < keypoints2[c]['Y'] + 0.1 and \
+                        keypoints2[c]['Z'] - 0.4 < keypoints_movel2[c]['Z'] < keypoints2[c]['Z'] + 0.4:
+                    keypoints2[c]['X'] = keypoints_movel2[c]['X']
+                    keypoints2[c]['Y'] = keypoints_movel2[c]['Y']
+                    keypoints2[c]['Z'] = keypoints_movel2[c]['Z']
                 else:
-                    sair = True
+                    sair2 = True
 
-        # for c in range(0, len(keypoints)):
-        #     if keypoints[c]['X'] != keypoints_movel[c]['X']:
-        #         sair = True
-
-
-        if sair == True:
+        if sair2 is True:
+            print('O JOGADOR 2 PERDEU!!')
             break
-
-            # print('-----------')
-            # print(keypoints[0]['X'])
-            #
-            # print(pos_nariz.x)
-            #
-            # print('-----------')
-            # if float(pos_nariz.x) != float(keypoints[0]['X']):
-            #     break
-            #
-            #
-            # elif keypoints[cont]['X'] - 0.1 < data_point.x < keypoints[cont]['X'] + 0.1 and keypoints[cont]['Y'] - 0.1 \
-            #         < data_point.y < keypoints[cont]['Y'] + 0.1 and keypoints[cont]['Z'] - 0.1 < data_point.z \
-            #         < keypoints[cont]['Z'] + 0.1:
-            #
-            #     keypoints[cont]['X'] = data_point.x
-            #     keypoints[cont]['Y'] = data_point.y
-            #     keypoints[cont]['Z'] = data_point.z
-            #
-            #
-            #
-            # cont += 1
-        print('--------------')
-        print('keypoint: \t\t', keypoints[0]['X'])
-        print('keypoints movel: ', keypoints_movel[0]['X'])
-        print('--------------')
-
 
         sleep(0.3)
         cv2.imshow('Mediapipe Feed1', image1)
-        cv2.imshow('Mediapipe Feqed2', image2)
+        cv2.imshow('Mediapipe Feed2', image2)
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
-        cont += 1
 
 
 cap1.release()
